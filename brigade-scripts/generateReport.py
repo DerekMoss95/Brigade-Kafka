91553125f095f159c417a8f1a5b610951677c4bb
DerekMoss95 <mossderek88@gmail.com>
2019-02-21 17:50:39 -0700

    Got the python script to grab data form ridb.recreation.gov api and print it

--- brigade-scripts/generateReport.py
+++ brigade-scripts/generateReport.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+import requests
 
-print("hello")
+name = "derek"
+email = "pidgeotteam@gmail.com"
+campsiteName = "bear"
+URL = "https://ridb.recreation.gov/api/v1/campsites?query="+campsiteName+"&limit=5&offset=0"
+
+print(URL)
+data = {'accept': 'application/json', 'apikey': '77567128-da03-485c-b324-f8d2a0059b9d'}
+
+r = requests.get(url = URL, params = data)
+jsondata = r.json() 
+
+#campName = data['RECDATA'][0]
+
+print(jsondata)

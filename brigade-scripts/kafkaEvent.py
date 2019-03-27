5905b880d0b6c1f32e8ece5c6a38dba7bd02102e
DerekMoss95 <mossderek88@gmail.com>
2019-03-27 16:13:20 -0600

    Made it so that server.py is ready to be used in a real kubernetes cluster or a local kube config like ours. Also gave server.py argparse so that it can parse the arguments given to it by helm in the future. Need to keep testing it when spinning up a container locally without helm and see how to make it work

--- brigade-scripts/kafkaEvent.py
+++ brigade-scripts/kafkaEvent.py
@@ -5,7 +5,6 @@ import os, uuid, base64, json, subprocess
 from kubernetes import client, config
 from json import JSONEncoder
 
-# I just created this function to follow the Jupyter workbook that matt sent us. It creates a scret but is still missing a couple attributes for the brigade.js file to pick it up. I play with adding those later
 
 def createSecretPython(payload):
     config.load_kube_config()
@@ -13,27 +12,25 @@ def createSecretPython(payload):
     api_instance = client.CoreV1Api()
     sec = client.V1Secret()
     UUID = str(uuid.uuid4()) 
-    # Check to see if project name works or if we need to change to projectid
     sec.metadata = client.V1ObjectMeta(name="mysecret" + str(UUID), 
             labels={"heritage":"brigade", 
-                "project":"brigade-kafka", 
+                "project":"brigade-kafka", #******
                 "build": "mysecret" + str(UUID), 
                 "component":"build"})
     sec.type = "brigade.sh/build"
-    encoded_data = base64.b64encode(json.dumps(payload).encode())
-    decoded_data = encoded_data.decode('utf-8')
-    json_data = {"payload": decoded_data}
+    encoded_payload = base64.b64encode(json.dumps(payload).encode())
+    decoded_payload = encoded_payload.decode('utf-8')
+    json_data = {"payload": decoded_payload}
     sec.data = json_data
     sec.string_data = {
         "event_type": "exec", 
         "build_id": "mysecret" + str(UUID), 
-        "commit_ref": "master", 
-        "project": "brigade-kafka", 
-        "project_id": "brigade-kafka",
+        "commit_ref": "master", #*******
+        "project": "brigade-kafka", #******
+        "project_id": "brigade-kafka", #******
 		"build_name": "mysecret" + str(UUID),
 		"event_provider": "brigade_cli"}
-    #why are AppData and payload data secrets available in the mysecret pod but not in the generatereport pod?
     api_instance.create_namespaced_secret(namespace="default", body=sec)
 
 if __name__ == '__main__':
-    createSecretPython("Hello World")
+    createSecretPython("")
--- brigade-scripts/server.py
+++ brigade-scripts/server.py
@@ -1,15 +1,63 @@
 #!/usr/bin/python3
 from kafka import KafkaConsumer
-import kafkaEvent
-import subprocess
-import time
-topic = 'pidgeot'
-serverIP = '192.168.70.3:9092'
-offset = 'latest'
+from kubernetes import client, config
+from json import JSONEncoder
+import subprocess, time, os, uuid, base64, subprocess, json, sys, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--topic', '-topic', '-TOPIC', required=True)
+parser.add_argument('--brigade_project_name', '-brigade_project_name', '-BRIGADE_PROJECT_NAME', required=True)
+parser.add_argument('--host', '-host', '-HOST', required=True)
+parser.add_argument('--port', '-port', '-PORT', required=True)
+parser.add_argument('--offset', '-offset', '-OFFSET', required=True)
+args = parser.parse_args()
+print(args)
+
+topic = args.topic
+serverIP = args.host + ':' + args.port
+offset = args.offset
+
+#topic = 'pidgeot'
+#serverIP = '192.168.70.3:9092'
+#offset = 'latest'
+
 consumer = KafkaConsumer(topic, bootstrap_servers=serverIP, auto_offset_reset=offset)
 
+# Kafka Custom Event Gateway
+def createSecretPython(payload):
+    client.configuration.assert_hostname = False
+    api_instance = client.CoreV1Api()
+    sec = client.V1Secret()
+    UUID = str(uuid.uuid4()) 
+    sec.metadata = client.V1ObjectMeta(name="mysecret" + str(UUID), 
+            labels={"heritage":"brigade", 
+                "project":args.brigade_project_name,
+                "build": "mysecret" + str(UUID), 
+                "component":"build"})
+    sec.type = "brigade.sh/build"
+    encoded_payload = base64.b64encode(json.dumps(payload).encode())
+    decoded_payload = encoded_payload.decode('utf-8')
+    json_data = {"payload": decoded_payload}
+    sec.data = json_data
+    sec.string_data = {
+        "event_type": "exec", 
+        "build_id": "mysecret" + str(UUID), 
+        "commit_ref": "master",
+        "project_id": args.brigade_project_name,
+		"build_name": "mysecret" + str(UUID),
+		"event_provider": "brigade_cli"}
+    api_instance.create_namespaced_secret(namespace="default", body=sec)
+
 if __name__ == '__main__':
+    try:
+        config.load_incluster_config()
+    except:
+        config.load_kube_config()
     for message in consumer:
         print (message)
         messagestr = message.value
-        kafkaEvent.createSecretPython(ascii(messagestr))
+        createSecretPython(ascii(messagestr))
+
+
+
+
